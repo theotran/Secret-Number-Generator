6c08077cebb81315fc64887688bf8fa4545930af
theo tran <theotran@rocketmail.com>
2015-11-05 21:32:21 -1000

    Finished the assignment and passed the tests

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,15 @@
+var min = 0;
+var max = 1000000;
+var number = 0;
+
 'use strict';
 module.exports = function() {
-  
+  	var number = Math.random() * (max - min) + min;
+ 
+  var secretNumber = function (min, max) {
+  	
+  	return number;
+    
+  };
+	return secretNumber;
 };
